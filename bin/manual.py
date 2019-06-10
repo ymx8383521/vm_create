8b1592f6bbf31831147109289980f381aaa49a5d
yangmengxue <mengxue.yang@iichain.io>
2019-06-10 10:05:02 +0800

    手动安装打印输出

--- bin/manual.py
+++ bin/manual.py
@@ -12,7 +12,8 @@ import json
 
 def run():
     logger=my_logger()
-    with open('../manual_vm.json','r',encoding='utf-8') as f:
+    conf_path = os.path.join(BASE_DIR, 'manual_vm.json')
+    with open(conf_path,'r',encoding='utf-8') as f:
         res=f.read()
     res=json.loads(res)
     # print(type(res),res.get('results'))
@@ -21,7 +22,7 @@ def run():
         logger.debug(install_li)
         if install_li:
             for item in install_li:
-                starting=True
+                # starting=True
                 try:
                     if item["room_name"] == 'yi':
                         vm_obj=AutoVM(settings.FVCENTER_USER,settings.FVCENTER_PASSWORD,settings.FVCENTER_IP,item["room_name"],item["host_ip"],item["datastore"],item["vm_ip"],item["vm_gateway"],item["vm_cpu"],item["vm_memory"],item["vm_disk"],item["vm_name"],item["vm_proposer"],item["id"])
@@ -35,14 +36,17 @@ def run():
                     else:
                         raise Exception('机房名称错误')
                     # 创建虚拟机
+                    print('%s正在创建中' % item["vm_ip"])
                     vm_obj.create_VM()
                     # 启用cpu mem热插拔
                     vm_obj.hot_plug()
                     # 设置清理脚本断开cdrom功能
                     set_discdrom(item["room_name"],item["vm_name"])
                     # 打包镜像
+                    print("打包镜像")
                     vm_obj.iso_pac()
                     # 上传镜像
+                    print("上传镜像")
                     up=vm_obj.upload_iso(settings.ISO_LOCAL)
                     if up:
                         # 开机自动安装
@@ -60,6 +64,7 @@ def run():
                     #     else:
                     #         time.sleep(10)
                     #         count += 1
+                    print('Successful 机房:%s 虚拟机:%s 装机中...请稍后再试'%(item["room_name"],item["vm_name"]))
                     logger.info('Successful 机房:%s 虚拟机:%s 装机中...请稍后再试'%(item["room_name"],item["vm_name"]))
                 except Exception as e:
                     logger.error('机房:%s 虚拟机:%s 装机失败，请查看失败原因并手动删除主机 %s'%(item["room_name"],item["vm_name"],e))
